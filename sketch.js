eda14a2f4e2d81b3848afe0f906b8f597b00fa08
Mmcm2 <mateus.caracciolo@gmail.com>
2019-11-12 11:09:23 -0300

    castanha da pará

--- sketch.js
+++ sketch.js
@@ -153,6 +153,36 @@ function mouseReleased() {
     //console.log('released');
 }
 
+function castanhaDoPara(points){
+  let curve = [];
+  var x = 0;
+  var inv = 1/numAv;
+  let aux = [];
+  for(let i = 0; i < numAv; i++){
+    x  = i*inv;
+    for(let j = 0; j < points.length; j++){
+      for(let k = 0; k < points.length - j; k++){
+        if(j == 0){
+          
+          aux[k] = createVector(points[k].x, points[k].y);
+        }
+        else{
+         aux[k].x = aux[k].x*(1-x) + aux[k+1].x*x;
+         aux[k].y = aux[k].y*(1-x) + aux[k+1].y*x;
+        }
+      }
+    }
+    curve[i] = createVector(aux[0].x, aux[0].y);
+  }
+  return curve;
+}
+
+function drawCurve(curve){
+    for(let i = 0; i < curve.length - 1; i++){
+       line(curve[i].x, curve[i].y, curve[i+1].x, curve[i+1].y);
+    }
+}
+
 function draw() {
     if(clickedLastDraw){
         background(255,255,255);
@@ -193,6 +223,11 @@ function draw() {
     }
   
     for(var i = 0;i < points.length;i++){
+        if(printCurve){
+          if(points[i].length >= 3){
+                drawCurve(castanhaDoPara(points[i]));
+            }
+        }
         for(var j = 0;j < points[i].length;j++){
             var x = points[i][j].x;
             var y = points[i][j].y;
